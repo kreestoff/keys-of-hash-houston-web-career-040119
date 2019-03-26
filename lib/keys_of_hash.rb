15698d96757c80746bcf1eec47d6ab9a97b70ee0
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-26 17:04:48 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |animal, location|
+      arguments.each do |i|
+        if i == location
+          array.push(key)
+        end
+      end
+    end
+    array
   end
 end
